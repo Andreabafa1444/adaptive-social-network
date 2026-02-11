b6bf667c50270564adc965e4ff6b43091fde28db
Andrea1444 <al331696@edu.uaa.mx>
2026-02-10 22:03:33 -0600

    PWA OFICIALMENTE

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", (event) => {
+    console.log("Service Worker instalado");
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    // Básico por ahora
+  });
+  
